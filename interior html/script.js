6c0514e881da953ef407c1245b25afbcc00dd262
aidpotato <123840074+aidpotato@users.noreply.github.com>
2025-04-22 16:44:56 -0700

    Delete interior html directory

--- interior html/script.js
+++
@@ -1,9 +0,0 @@
-function swapImage(projectId, beforeImage, afterImage) {
-    const imgElement = document.getElementById(projectId);
-    
-    if (imgElement.src.includes(beforeImage)) {
-        imgElement.src = afterImage;
-    } else {
-        imgElement.src = beforeImage;
-    }
-}
